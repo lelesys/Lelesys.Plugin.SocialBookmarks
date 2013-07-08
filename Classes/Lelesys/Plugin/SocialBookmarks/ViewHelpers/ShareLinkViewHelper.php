c06076045783347c5c1942d0f723ba8f7a3c17fb
Gauri Shirodkar <gauri.shirodkar@lelesys.com>
2013-07-08 16:58:36 +0530

    [TASK] Updated documentation of class file.

--- Classes/Lelesys/Plugin/SocialBookmarks/ViewHelpers/ShareLinkViewHelper.php
+++ Classes/Lelesys/Plugin/SocialBookmarks/ViewHelpers/ShareLinkViewHelper.php
@@ -3,7 +3,7 @@
 namespace Lelesys\Plugin\SocialBookmarks\ViewHelpers;
 
 /*                                                                                *
- * This script belongs to the TYPO3 Flow package "Lelesys.Plugin.SocialBookmarks".*
+ * This script belongs to the package "Lelesys.Plugin.SocialBookmarks".           *
  *                                                                                *
  * It is free software; you can redistribute it and/or modify it under            *
  * the terms of the GNU Lesser General Public License, either version 3           *
